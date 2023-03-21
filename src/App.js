afcc44143d9450cb9f373869d34e5715124de090
unknown <souravnegi144@gmail.comm>
2023-03-21 11:07:11 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router , Routes, Route } from 'react-router-dom'
+import First from './pages/First'
+import Second from './pages/Second'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Router >
+      <Routes>
+        <Route path='/' element={<First />} />
+        <Route path='/second' element={<Second />} />
+      </Routes>
+    </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
